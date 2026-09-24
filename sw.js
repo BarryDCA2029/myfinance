43590fb9cb5d9cf81e8caf65a81c86e86c94a7ef
BarryDCA2029 <s5y8h9njyc@privaterelay.appleid.com>
2026-09-25 06:05:46 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='my-finance-v1.3.0';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./baac-icon.png'];
+const CACHE='my-finance-v1.2';
+const ASSETS=['./','./index.html','./styles.css?v=1.2','./app.js?v=1.2','./manifest.webmanifest','./icon-192.png','./icon-512.png','./bofa.png','./scb.png','./ktb.png','./kbank.png','./gsb.png','./ttb.png','./bbl.png','./honda.png','./mitsubishi.png','./gpf.png','./gold.png','./land.png','./house.png','./vehicle.png','./other.png','./wallet.png','./fund.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
